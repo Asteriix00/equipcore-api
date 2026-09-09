0ba1ac615374125aa8046ed54b5b8904c693588b
Amine CHAKHAR <asterix0_0@192.168.0.102>
2026-09-09 22:37:42 +0100

    feat: add Flyway migrations and configure Spring profiles for dev, docker and test

---
+++ src/test/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO categories (id, name)
+VALUES (RANDOM_UUID(), 'LAPTOP'),
+       (RANDOM_UUID(), 'DESKTOP'),
+       (RANDOM_UUID(), 'MONITOR'),
+       (RANDOM_UUID(), 'KEYBOARD'),
+       (RANDOM_UUID(), 'MOUSE'),
+       (RANDOM_UUID(), 'SMARTPHONE'),
+       (RANDOM_UUID(), 'TABLET'),
+       (RANDOM_UUID(), 'PROJECTOR'),
+       (RANDOM_UUID(), 'PRINTER'),
+       (RANDOM_UUID(), 'HEADSET');
